b916664a885d2d0bcc1bddf33f6c9847925748f6
Steve Lacey <steve@stevelacey.net>
2010-11-20 02:25:54 +0000

    Implemented storage of extra user variables to sfGuardUser and sfDoctrineGuardUserProfile object. Switched existing user check to use findById method: usernames are subject to change, ids are not.

--- modules/sfTwitterAuth/actions/actions.class.php
+++ modules/sfTwitterAuth/actions/actions.class.php
@@ -1,9 +1,7 @@
 <?php
 
-class sfTwitterAuthActions extends sfActions
-{
-  public function executeLogin(sfRequest $request)
-  {
+class sfTwitterAuthActions extends sfActions {
+  public function executeLogin(sfRequest $request) {
     /* Consumer key from twitter */
     $consumer_key = sfConfig::get('app_sf_twitter_auth_consumer_key');
     $consumer_secret = sfConfig::get('app_sf_twitter_auth_consumer_secret');
@@ -39,12 +37,9 @@ class sfTwitterAuthActions extends sfActions
         break;
       case 'returned':
         /* If the access tokens are already set skip to the API call */
-        if ((!$user->getAttribute('sfTwitterAuth_oauth_access_token')) &&   (!$user->getAttribute('sfTwitterAuth_oauth_access_token_secret')))
-        {
+        if ((!$user->getAttribute('sfTwitterAuth_oauth_access_token')) &&   (!$user->getAttribute('sfTwitterAuth_oauth_access_token_secret'))) {
           /* Create TwitterOAuth object with app key/secret and token key/secret from default phase */
-          $to = new TwitterOAuth($consumer_key, $consumer_secret,
-            $user->getAttribute('sfTwitterAuth_oauth_request_token'),
-            $user->getAttribute('sfTwitterAuth_oauth_request_token_secret'));
+          $to = new TwitterOAuth($consumer_key, $consumer_secret, $user->getAttribute('sfTwitterAuth_oauth_request_token'), $user->getAttribute('sfTwitterAuth_oauth_request_token_secret'));
           /* Request access tokens from twitter */
           $tok = $to->getAccessToken($request->getParameter('oauth_verifier'));
           /* Save the access tokens. These could be saved in a database as they don't
@@ -53,31 +48,19 @@ class sfTwitterAuthActions extends sfActions
           $user->setAttribute('sfTwitterAuth_oauth_access_token_secret', $tok['oauth_token_secret']);
         }
         /* Create TwitterOAuth with app key/secret and user access key/secret */
-        $to = new TwitterOAuth($consumer_key, $consumer_secret,
-          $user->getAttribute('sfTwitterAuth_oauth_access_token'),
-          $user->getAttribute('sfTwitterAuth_oauth_access_token_secret'));
+        $to = new TwitterOAuth($consumer_key, $consumer_secret, $user->getAttribute('sfTwitterAuth_oauth_access_token'), $user->getAttribute('sfTwitterAuth_oauth_access_token_secret'));
         /* Run request on twitter API as user. */
-        $result = $to->OAuthRequest('https://twitter.com/account/verify_credentials.xml', array(), 'GET');
-        $xml = simplexml_load_string($result);
-        $username = $xml->xpath("descendant::screen_name");
-        if (is_array($username) && count($username))
-        {
-          $username = (string) $username[0];
-          $guardUser = Doctrine_Query::create()->from('sfGuardUser u')->where('username = ?', array($username))->fetchOne();
-          if (!$guardUser)
-          {
+        $result = $to->OAuthRequest('https://twitter.com/account/verify_credentials.xml', 'GET', array());
+        $xml = new SimpleXMLElement($result);
+
+        if ($xml->id) {
+          $guardUser = Doctrine::getTable('sfGuardUser')->findOneById($xml->id);
+
+          if (!$guardUser) {
             // Make a new user here
-            $guardUser = new sfGuardUser();
-            $guardUser->setUsername($username);
-            // Set a secure, random sfGuard password to ensure that this
-            // account is not wide open if conventional logins are permitted
-            $guid = "";
-            for ($i = 0; ($i < 8); $i++) {
-              $guid .= sprintf("%x", mt_rand(0, 15));
-            }
-            $guardUser->setPassword($guid);
-            $guardUser->save();            
+            $guardUser = $this->createUser($xml);
           }
+
           $user->signIn($guardUser);
 
           // always redirect to a URL set in app.yml
@@ -97,8 +80,53 @@ class sfTwitterAuthActions extends sfActions
         break;
     }
   }
-  public function executeFailed()
-  {
-    
+
+  private function createUser(SimpleXMLElement $xml) {
+    $user = new sfGuardUser();
+    $user->setId((int) $xml->id);
+    $user->setUsername($xml->screen_name);
+    $user->setEmailAddress($xml->screen_name);
+    $user->setFirstName(substr($xml->name, 0, strpos($xml->name, ' ')));
+    $user->setLastName(substr($xml->name, strpos($xml->name, ' ') + 1, strlen($xml->name)));
+    $user->setPassword($this->generatePassword());
+
+    $profile = new sfGuardUserProfile();
+    $profile->setId((int) $xml->id);
+    $profile->setName($xml->name);
+    $profile->setDescription($xml->description);
+    $profile->setWebsite($xml->url);
+    $profile->setImage($xml->profile_image_url);
+
+    $user->setProfile($profile);
+    $user->save();
+    return $user;
+  }
+
+  private function generatePassword() {
+    // Set a secure, random sfGuard password to ensure that this
+    // account is not wide open if conventional logins are permitted
+    $guid = '';
+
+    for ($i = 0; ($i < 8); $i++) {
+      $guid .= sprintf("%x", mt_rand(0, 15));
+    }
+
+    return $guid;
+  }
+
+  public function executeLogout(sfRequest $request) {
+    $user = $this->getUser(); /* @var $user myUser */
+    $user->signOut();
+
+    // always redirect to a URL set in app.yml
+    // or to the referer
+    // or to the homepage
+    $signoutUrl = sfConfig::get('app_sf_twitter_auth_success_signout_url', $user->getReferer($request->getReferer()));
+
+    return $this->redirect('' != $signoutUrl ? $signoutUrl : '@homepage');
+  }
+
+  public function executeFailed() {
+
   }
 }
