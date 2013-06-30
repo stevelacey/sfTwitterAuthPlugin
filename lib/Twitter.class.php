fe61396fd2bafc281c8546991d442fcfb47a0fde
Steve Lacey <steve@stevelacey.net>
2013-06-30 16:21:28 +0100

    Allow specification of default OAuth creds to perform requests using if no authenticated user is present.

--- lib/Twitter.class.php
+++ lib/Twitter.class.php
@@ -4,14 +4,15 @@ class Twitter {
   private static $connection = null;
 
   public static function getConnection() {
-    $user = sfContext::getInstance()->getUser();
-    
-    if (!$user->isAuthenticated()) {
-      return false;
-    }
+    $user = sfContext::hasInstance() && sfContext::getInstance()->getUser()->isAuthenticated() ? sfContext::getInstance()->getUser() : false;
 
     if(self::$connection === null) {
-      self::$connection = new TwitterOAuth(sfConfig::get('app_sf_twitter_auth_consumer_key'), sfConfig::get('app_sf_twitter_auth_consumer_secret'), $user->getAttribute('oauth_access_token', null, 'sfTwitterAuth'), $user->getAttribute('oauth_access_token_secret', null, 'sfTwitterAuth'));
+      self::$connection = new TwitterOAuth(
+        sfConfig::get('app_sf_twitter_auth_consumer_key'),
+        sfConfig::get('app_sf_twitter_auth_consumer_secret'),
+        $user ? $user->getAttribute('oauth_access_token', null, 'sfTwitterAuth') : sfConfig::get('app_sf_twitter_auth_access_token') ,
+        $user ? $user->getAttribute('oauth_access_token_secret', null, 'sfTwitterAuth') : sfConfig::get('app_sf_twitter_auth_access_token_secret')
+      );
     }
 
     return self::$connection;
